b835cb9d5ba0e042c9ebef7f5a6e4b8ff4dde21c
peketo231 <peketo231@gmail.com>
2021-07-07 21:00:04 +0900

    Add: バリデーションの追加

--- app/models/battle_record.rb
+++ app/models/battle_record.rb
@@ -2,5 +2,17 @@ class BattleRecord < ApplicationRecord
   belongs_to :user
   belongs_to :winning_eleven
 
-  validates :rate, presence: true
+  validates :rate, numericality: { less_than_or_equal_to: 9999 }
+  validates :win_rate, numericality: { less_than_or_equal_to: 100 }, allow_blank: true, format: { with: /\A[0-9]+\.[0-9]\z/ }
+
+  with_options uniqueness: { scope: :winning_eleven_id }, if: :past? do
+    validates :rate
+    validates :win_rate
+  end
+
+  def past?
+    return unless winning_eleven_id.present?
+
+    winning_eleven.series_status == 'past'
+  end
 end
