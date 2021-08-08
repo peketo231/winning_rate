1bfcd338c503e09547ed84e72c15a6b0d9fc24ab
peketo231 <peketo231@gmail.com>
2021-08-08 17:00:04 +0900

    Fix: データの修正

--- app/controllers/monthlies_controller.rb
+++ app/controllers/monthlies_controller.rb
@@ -2,6 +2,7 @@ class MonthliesController < ApplicationController
   def index
     @winning_eleven = WinningEleven.find_by(title: params[:label])
     @battle_record = current_user.battle_records.where(winning_eleven_id: @winning_eleven)
-    @data = @battle_record.joins(:monthlies)
+    @month = Monthly.where(battle_record_id: @battle_record)
+    @data = @month.distinct.pluck(:month)
   end
 end
