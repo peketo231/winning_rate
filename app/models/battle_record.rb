de3b9b7374a8b2e9f51dbc5bef4e0c66d744750c
peketo231 <peketo231@gmail.com>
2021-10-04 19:00:04 +0900

    Fix: 修正漏れを対応

--- app/models/battle_record.rb
+++ app/models/battle_record.rb
@@ -2,6 +2,6 @@ class BattleRecord < ApplicationRecord
   belongs_to :user
   belongs_to :winning_eleven
 
-  validates :rate, numericality: { less_than_or_equal_to: 9999 }
-  validates :win_rate, numericality: { less_than_or_equal_to: 100 }, allow_blank: true, format: { with: /\A[0-9]+\.[0-9]\z/ }
+  validates :rate, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 9999 }
+  validates :win_rate, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }, allow_nil: true, format: { with: /\A[0-9]+\.[0-9]\z/ }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :battle_records, dependent: :destroy
   has_many :winning_elevens, through: :battle_records
 
-  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, length: { minimum: 8 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
