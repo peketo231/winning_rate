94d555d75f40abe3c4cc05196fc93555fca3e7f5
peketo231 <peketo231@gmail.com>
2021-10-22 19:15:01 +0900

    Fix: callback_urlの修正

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -117,8 +117,8 @@ Rails.application.config.sorcery.configure do |config|
   #
   config.twitter.key = Rails.application.credentials.dig(:twitter, :key)
   config.twitter.secret = Rails.application.credentials.dig(:twitter, :secret_key)
-  config.twitter.callback_url = 'http://127.0.0.1:3000/oauth/callback?provider=twitter'
-  config.twitter.user_info_mapping = { name: 'name' }
+  config.twitter.callback_url = Rails.env.production? ? 'https://werate.herokuapp.com/oauth/callback?provider=twitter' : 'http://localhost:3000/oauth/callback?provider=twitter'
+  config.twitter.user_info_mapping = { email: 'screen_name' }
   #
   # config.facebook.key = ""
   # config.facebook.secret = ""
