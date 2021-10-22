0d54f4f6e7953f2d74eac8edab367da7e3e470a2
peketo231 <peketo231@gmail.com>
2021-10-22 16:45:02 +0900

    Add: タイムソーンを日本時刻に設定

--- config/application.rb
+++ config/application.rb
@@ -42,6 +42,8 @@ module WinningRate
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
 
+    config.time_zone = 'Tokyo'
+
     config.action_view.field_error_proc = proc do |html_tag, instance|
       if instance.is_a?(ActionView::Helpers::Tags::Label)
         html_tag.html_safe
